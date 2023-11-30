9f37f1685fe89aaa8f7aa5f881151284959873eb
Hyunjun Lee <hyunjunl@c2r3s4.42seoul.kr>
2023-11-30 17:04:11 +0900

    my mistake

--- ..h
+++
@@ -1,7 +0,0 @@
-#define show_res(res, prefix)								\
-	printf("\x1B[0;32m%i%s.OK \x1B[0m", g_test++, prefix);	\
-    fflush(stdout);
-char	*get_next_line(int fd);
-#ifndef FILE_UTILS_H
-# define get_next_line(fd) (read(fd, &fd, 1) != -42 && expectedReturn ? strdup(expectedReturn) : 0)
-#endif
