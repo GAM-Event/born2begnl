af96200646c5d24ac873120bad252761f52d8e27
sayoon <147060241+sayoon-mandarine@users.noreply.github.com>
2023-11-30 16:35:12 +0900

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sayoon <sayoon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 15:42:24 by sayoon            #+#    #+#             */
+/*   Updated: 2023/11/30 15:42:25 by sayoon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+# undef BUFFER_SIZE
+# define BUFFER_SIZE 1
+
+char	*get_next_line(int fd);
+#endif
